83f736fa811297702b8c91044f9012b09ee17361
Hiking <tom.xj.cheng@gmail.com>
2015-10-26 22:46:08 +0800

    A keyboard-controllable ball.

--- src/com/twopits/balls/BallsMain.java
+++ src/com/twopits/balls/BallsMain.java
@@ -6,6 +6,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,7 +22,7 @@ import javax.swing.WindowConstants;
 public class BallsMain extends JPanel {
 
 	private static Dimension mScreenDimen = new Dimension(500, 300);
-	private static long mLastFrameTime;
+	private static BallsKeyManager mKeyManager;
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
@@ -31,55 +35,109 @@ public class BallsMain extends JPanel {
 		BallsMain game = new BallsMain();
 		frame.add(game);
 		frame.setVisible(true);
+		mKeyManager = new BallsKeyManager();
+		frame.addKeyListener(mKeyManager);
 
-		mLastFrameTime = System.currentTimeMillis();
+		long lastFrameTime = System.currentTimeMillis();
 		while (true) {
-			game.moveBall();
-			game.repaint();
+			long currentTime = System.currentTimeMillis();
+			long dt = currentTime - lastFrameTime;
+			lastFrameTime = currentTime;
+			if (dt != 0) {
+				game.update(dt);
+			}
 		}
 	}
 
-	double x = 0, y = 0;
-	int speedX = 100, speedY = 100;
+	static class BallsKeyManager implements KeyListener {
 
-	private void moveBall() {
-		long currentTime = System.currentTimeMillis();
-		long dt = currentTime - mLastFrameTime;
-		mLastFrameTime = currentTime;
+		private List<Integer> mPressedKeys;
 
-		x += speedX * dt / 1000.0;
-		y += speedY * dt / 1000.0;
-
-		if (x + 30 > mScreenDimen.width) {
-			x -= (x + 30 - mScreenDimen.width) * 2;
-			speedX *= -1;
-		}
-		if (x < 0) {
-			x *= -1;
-			speedX *= -1;
+		public BallsKeyManager() {
+			mPressedKeys = new ArrayList<Integer>();
 		}
 
-		if (y + 30 > mScreenDimen.height) {
-			y -= (y + 30 - mScreenDimen.height) * 2;
-			speedY *= -1;
+		public boolean isKeyPressed(int keyCode) {
+			return mPressedKeys.contains(keyCode);
 		}
-		if (y < 0) {
-			y *= -1;
-			speedY *= -1;
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (!mPressedKeys.contains(e.getKeyCode())) {
+				mPressedKeys.add(e.getKeyCode());
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			mPressedKeys.remove((Integer) e.getKeyCode());
 		}
 	}
+
+	private static final int PLAYER_SPEED = 500;
+	private double mPlayerX = 0, mPlayerY = 0;
 
 	public BallsMain() {
+	}
 
+	private void update(long dt) {
+		double playerWayX = 0, playerWayY = 0;
+
+		if (mKeyManager.isKeyPressed(KeyEvent.VK_LEFT)) {
+			playerWayX -= 1.0;
+		}
+		if (mKeyManager.isKeyPressed(KeyEvent.VK_RIGHT)) {
+			playerWayX += 1.0;
+		}
+		if (mKeyManager.isKeyPressed(KeyEvent.VK_UP)) {
+			playerWayY -= 1.0;
+		}
+		if (mKeyManager.isKeyPressed(KeyEvent.VK_DOWN)) {
+			playerWayY += 1.0;
+		}
+
+		double unitFactor = Math.sqrt(playerWayX * playerWayX + playerWayY * playerWayY);
+		if (unitFactor != 0) {
+			playerWayX /= unitFactor;
+			playerWayY /= unitFactor;
+		}
+
+		mPlayerX += playerWayX * PLAYER_SPEED * dt / 1000.0;
+		mPlayerY += playerWayY * PLAYER_SPEED * dt / 1000.0;
+
+		if (playerWayX != 0 || playerWayY != 0) {
+			// Check out-of-bound
+			if (mPlayerX + 30 > mScreenDimen.width) {
+				mPlayerX = mScreenDimen.width - 30;
+			}
+			if (mPlayerX < 0) {
+				mPlayerX = 0;
+			}
+			if (mPlayerY + 30 > mScreenDimen.height) {
+				mPlayerY = mScreenDimen.height - 30;
+			}
+			if (mPlayerY < 0) {
+				mPlayerY = 0;
+			}
+
+			repaint();
+		}
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
+
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.fillOval((int) x, (int) y, 30, 30);
-		g2d.drawString("Position = " + String.format("(%d,%d)", (int) x, (int) y), 10, 250);
-		g2d.drawString("Time = " + System.currentTimeMillis(), 10, 270);
+
+		g2d.fillOval((int) mPlayerX, (int) mPlayerY, 30, 30);
+		g2d.drawString("Position: " + String.format("(%d,%d)", (int) mPlayerX, (int) mPlayerY), 10,
+				270);
+		g2d.drawString("Time stamp: " + System.currentTimeMillis(), 10, 290);
 	}
 }
