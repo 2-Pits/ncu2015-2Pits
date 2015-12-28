ca9a8b203d2edf7cae051c560a9c05f3b4d89539
arthur3791 <turtle45368@gmail.com>
2015-12-28 15:17:02 +0800

    TCP wait 4 gamer

--- client/src/com/twopits/balls/TCPCM.java
+++ client/src/com/twopits/balls/TCPCM.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.twopits.balls.libs.OneGamer;
 import dom.DynamicObjectModule;
 
-import javax.jws.Oneway;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -23,23 +22,20 @@ public class TCPCM {
     private InetAddress ServerIP;
     private Socket socket;
     private Thread recieveThread, sendThread;
-    private InputStream in ;
+    private InputStream in;
     private OutputStream out;
     private BufferedReader br;
     private InputStreamReader isr;
 
-
-
-    public TCPCM(App app,DynamicObjectModule dom){
+    public TCPCM(App app, DynamicObjectModule dom) {
         this.dom = dom;
         this.app = app;
-
     }
 
-    public void buildConnection(){
+    public void buildConnection() {
         try {
             ServerIP = InetAddress.getByName(IP);
-            socket = new Socket(ServerIP,PORT);
+            socket = new Socket(ServerIP, PORT);
             System.out.println("Create Socket");
             initAll();
             createThread();
@@ -50,30 +46,54 @@ public class TCPCM {
         }
     }
 
-    private void createThread(){
+    //等待初始指令
+    public void startrecieveInitThread(){
+
+    }
+    //等待開始遊戲
+    public void startrecieveStartGameThread(){
+
+    }
+    //請求GetBall指令
+
+    public void requestGetBall(String s){
+
+    }
+    //接收球狀態
+    public void startUpdateRecieveBallStatus(){
+
+    }
+
+    private void createThread() {
         recieveThread = new Thread(recieve);
         sendThread = new Thread(send);
 
         recieveThread.start();
     }
 
-    private void firstCall(){
+    private void firstCall() {
         String s;
+        String gball;
         OneGamer one;
         try {
             s = br.readLine();
-            one = new Gson().fromJson(s,OneGamer.class);
-            dom.initMyCharacter(one.getID(), one.getX(),one.getY());
-            app.getSceneRenderEngine().setPlayerPosition(one.getX(),one.getY());
-            System.out.println("ID = "+one.getID()+" X= " +one.getX()+" Y = " + one.getY());
+            System.out.println(s);
+            one = new Gson().fromJson(s, OneGamer.class);
+            dom.initMyCharacter(one.getID(), one.getX(), one.getY());
+            app.getSceneRenderEngine().setPlayerPosition(one.getX(), one.getY());
+            System.out.println("ID = " + one.getID() + " X= " + one.getX() + " Y = " + one.getY());
+            gball = br.readLine();
+            System.out.println(gball);
+            dom.updateBall(gball);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
 
-    private void initAll(){
+
+
+    private void initAll() {
         try {
             in = socket.getInputStream();
             out = socket.getOutputStream();
@@ -95,17 +115,36 @@ public class TCPCM {
 
     Runnable recieve = new Runnable() {
 
+        boolean isStart = false;
+        boolean isStop = false;
+        int clientCount;
+        String s;
 
         @Override
         public void run() {
 
+            if (!isStart) {
                 try {
+                    while (clientCount <= 4) {
+                        clientCount = br.read();
+                        System.out.println("Client Count = " + clientCount);
+                        if (clientCount == 4) {
+                            app.getRenderThread().start();
+                            isStart = true;
+                            break;
+                        }
 
-
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+            } else {
+
+                while (!isStop) {
+
+                }
+            }
 
         }
     };
